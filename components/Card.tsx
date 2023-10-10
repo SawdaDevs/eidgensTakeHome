44a3aad3d731c7bd115eb5e0d29def73babc2fc5
SawdaDevs <sawda.onanuga@gmail.com>
2023-10-10 14:09:15 +0100

    styled card for individual page

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,10 +12,10 @@ export default function Card(props: {productInfo:Product} ) {
         isHome ?
         <div>
             <Link href={`/product/${props.productInfo.id}`}>
-            <div className=" flex flex-col w-52 h-60 border  border-gray-300 rounded-xl p-2 min-w-fit shadow shadow-gray-300 hover:shadow-md hover:shadow-purple-800/70 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 relative"
+            <div className=" flex flex-col w-52 h-60 border border-gray-300 rounded-xl p-2 min-w-fit shadow shadow-gray-300 hover:shadow-md hover:shadow-purple-800/70 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 relative"
             >
                 <div className="h-1/6 p-1 justify-start">
-                    <h2 className="text-gray-600 justify-start line-clamp-1 text-2xl font-semibold indent-0.5 p-1">
+                    <h2 className="text-gray-600 justify-start line-clamp-1 text-2xl font-semibold drop-shadow-md  indent-0.5 p-1">
                         {props.productInfo.title}
                     </h2>
                 </div>
@@ -42,34 +42,42 @@ export default function Card(props: {productInfo:Product} ) {
         </div>
         :
         <>
-            <div className=" aspect-square border rounded-lg text-blue-900 border-violet-500/20 p-1">
-                <div className="h-1/6 p-1">
-                    <h2 className="justify-start text-2xl text-blue-900 font-semibold indent-0.5 p-1">
+            <div className=" flex flex-col w-100 min-w-fit min-h-fit h-5/6 border border-gray-300 rounded-xl shadow-md p-1 text-ellipsis overflow-hidden">
+                <div className="h-1/6 p-1  text-gray-600 flex flex-row justify-between">
+                <div className="p-3 w-2/3 ">
+                    <h1 className="justify-start text-3xl drop-shadow-md font-semibold indent-0.5">
                         {props.productInfo.title}
-                    </h2>
-                    <h3>
-                        Category: {props.productInfo.category}
-                    </h3>
-                    <h3>
-                        Rating {props.productInfo.rating}
-                    </h3>
-                    <h3>
-                        Rating {props.productInfo.price} + {props.productInfo.discountPercentage}
+                    </h1>
+                    <h3 className="text-xs font-thin indent-1">
+                    {props.productInfo.brand}
                     </h3>
                 </div>
-                <div className="h-1/6 p-1">
-                    <p className=" text-sm font-thin  text-blue-900 leading-snug p-1">
+                <div className="p-3 w-1/3 border rounded-xl border-gray-100/50 flex place-self-center shadow">
+                    <h3 className="p-3 text-right">
+                        Price {props.productInfo.price} + {props.productInfo.discountPercentage}%
+                    </h3>
+                </div>
+
+                </div>
+                <div className="h-1/6 p-2 text-gray-800 ">
+                    <p className=" text-md font-light p-3 text-center">
                         {props.productInfo.description}
                     </p>
+                    <p className=" text-md font-light text-right p-3">
+                        Rating: {props.productInfo.rating}/5
+                    </p>
                 </div>
-                <div className=" h-4/6 p-1.5">
-                    <Image 
+                <div className=" h-4/6 w-full flex justify-center justify-items-center relative p-3">
+                <div className="w-full h-full relative p-1 img-wrapper">
+                    <Image className="max-h-full rounded"
                         alt={props.productInfo.title}
-                        height={100}
+                        fill={true}
                         src={props.productInfo.thumbnail}
-                        width={40}
+                        quality={100}
+                        sizes="max-h-full"
                     />
                 </div>
+            </div>
             </div>
             
         </>
