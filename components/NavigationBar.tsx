41bf173059b62a5d7b264b7679463215bbe92f9b
SawdaDevs <sawda.onanuga@gmail.com>
2023-10-10 02:34:34 +0100

    navigation bar styling, title

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -6,7 +6,7 @@ export default function NavigationBar() {
         <div className='h-20 min-w-fit max-w-full bg-violet-900/75 border-4 border-gray-300 rounded-full'>
            <div className=" w-full h-full p-4 flex justify-between items-center">
               <Link href="/">
-                <h1 className ="text-white text-xl font-bold text-center"> Eidgens Take Home Challenge</h1>
+                <h1 className ="text-white text-4xl font-bold text-center"> Eidgens Take Home Challenge</h1>
               </Link>
               <Link href="/">
                 <p className="hidden md:flex gap-x-6 text-white">
